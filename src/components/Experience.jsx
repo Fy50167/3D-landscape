d40f226e4263fa4c29c7bbfc16d8ef44e95ea736
Francis Yang <fy50167@gmail.com>
2024-01-26 15:38:29 -0800

    fixing basic mesh

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,11 @@
-import { MeshNormalMaterial } from 'three';
-
 export default function Experience() {
     return (
-        <mesh>
-            <boxBufferGeometry />
+        <mesh
+            rotation={[-0.5 * Math.PI, 0, 0]}
+            position={[0, -1, 0]}
+            receiveShadow
+        >
+            <boxGeometry />
             <meshNormalMaterial />
         </mesh>
     );
