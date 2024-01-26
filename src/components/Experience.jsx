a0b2693ef72465f1ef61f9ed105cac2f1b24ee43
Francis Yang <fy50167@gmail.com>
2024-01-26 15:44:35 -0800

    orbit controls

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,17 @@
+import { OrbitControls } from '@react-three/drei';
+
 export default function Experience() {
     return (
-        <mesh
-            rotation={[-0.5 * Math.PI, 0, 0]}
-            position={[0, -1, 0]}
-            receiveShadow
-        >
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <>
+            <OrbitControls />
+            <mesh
+                rotation={[-0.5 * Math.PI, 0, 0]}
+                position={[0, -1, 0]}
+                receiveShadow
+            >
+                <boxGeometry />
+                <meshNormalMaterial />
+            </mesh>
+        </>
     );
 }
