e42dcb4f640fa8121caa75640ac387e25f0a5e5d
Francis Yang <fy50167@gmail.com>
2024-01-26 19:45:51 -0800

    modularize buttons

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react';
+import Button from './Button';
 
 export default function Experience() {
+    const animations = [
+        'Animation 1',
+        'Animation 2',
+        'Animation 3',
+        'Animation 4',
+    ];
+
     return (
         <div className='animation-controls'>
-            <button className='btn'>Animation 1</button>
             <button className='btn'>Animation 2</button>
             <button className='btn'>Animation 3</button>
             <button className='btn'>Animation 4</button>
