93dd1a17abe676fa6aa76c4aff26e423eceec2ed
Francis Yang <fy50167@gmail.com>
2024-01-26 19:23:59 -0800

    create context

--- src/components/Woman.jsx
+++ src/components/Woman.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { useGLTF, useAnimations } from '@react-three/drei';
 
 export default function Woman(props) {
@@ -6,6 +6,10 @@ export default function Woman(props) {
     const group = useRef();
     const { nodes, materials, animations } = useGLTF('./models/woman.gltf');
     const { actions, names } = useAnimations(animations, group);
+
+    useEffect(() => {
+        actions[names[0]].reset().fadeIn(0.5).play();
+    }, []);
 
     return (
         <group ref={group} {...props} dispose={null}>
