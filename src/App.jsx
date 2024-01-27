ae1b45e0cd73d018196ee9d3abae3bdddce0afc3
Francis Yang <fy50167@gmail.com>
2024-01-26 18:55:30 -0800

    inspectingreact three fiber animation example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import Experience from './components/Experience';
+import { easing } from 'maath';
 
 function App() {
+    function Rig() {
+        return useFrame((state, delta) => {
+            easing.damp3(
+                state.camera.position,
+                [1 + state.mouse.x / 4, 1.5 + state.mouse.y / 4, 2.5],
+                0.2,
+                delta
+            );
+        });
+    }
+
     return (
-        <Canvas>
+        <Canvas shadows camera={{ position: [1, 1.5, 2.5], fov: 50 }}>
+            <ambientLight />
+            <directionalLight
+                position={[-5, 5, 5]}
+                castShadow
+                shadow-mapSize={1024}
+            />
             <Experience />
+            <mesh
+                rotation={[-0.5 * Math.PI, 0, 0]}
+                position={[0, -1, 0]}
+                receiveShadow
+            >
+                <planeGeometry args={[10, 10, 1, 1]} />
+                <shadowMaterial transparent opacity={0.2} />
+            </mesh>
+            <Rig />
         </Canvas>
     );
 }
